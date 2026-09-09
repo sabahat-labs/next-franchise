3f60d17610ab6a9cd6da95929c99c4b512f2957c
sabahatdpo <sabahatdpo.pbs@gmail.com>
2026-09-09 11:15:40 +0500

    base layrer switch

--- main.js
+++ main.js
@@ -7,10 +7,12 @@
 const map = L.map('map');
 map.setView([33.6900, 73.0400], 11);
 
-L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-    maxZoom: 16,
-    attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
-}).addTo(map);
+var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+	maxZoom: 16
+});
+
+Esri_WorldGrayCanvas.addTo(map);
 
 const outletIcon = L.divIcon({
     html: '<div style="background:#3A2E27;width:16px;height:16px;border-radius:50%;border:3px solid #fff;box-shadow:0 0 4px rgba(0,0,0,.5)"></div>',
